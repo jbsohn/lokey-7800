285ea99397a49d031ecd1ad94a711fe5e99e08fa
John Sohn <john.sohn@gmail.com>
2026-01-05 23:08:59 -0500

    Read A0–A7 and output to UART

--- test/pico/low-address/low-address.cpp
+++ test/pico/low-address/low-address.cpp
@@ -5,25 +5,31 @@
 constexpr uint TX_PIN = 0;
 constexpr uint RX_PIN = 1;
 
-constexpr uint8_t A0_PIN = 10;
-constexpr uint8_t A1_PIN = 11;
-constexpr uint8_t A2_PIN = 12;
-constexpr uint8_t A3_PIN = 13;
+// A0–A7, LSB → MSB
+constexpr std::array<uint8_t, 8> ADDR_PINS = {
+    10,  // A0
+    11,  // A1
+    12,  // A2
+    13,  // A3
+    14,  // A4
+    15,  // A5
+    16,  // A6
+    17   // A7
+};
 
 void init_addr_pins() {
-    gpio_init(A0_PIN);
-    gpio_init(A1_PIN);
-    gpio_init(A2_PIN);
-    gpio_init(A3_PIN);
-
-    gpio_set_dir(A0_PIN, GPIO_IN);
-    gpio_set_dir(A1_PIN, GPIO_IN);
-    gpio_set_dir(A2_PIN, GPIO_IN);
-    gpio_set_dir(A3_PIN, GPIO_IN);
+    for (const auto pin : ADDR_PINS) {
+        gpio_init(pin);
+        gpio_set_dir(pin, GPIO_IN);
+    }
 }
 
-uint8_t read_addr_nibble() {
-    return gpio_get(A3_PIN) << 3 | gpio_get(A2_PIN) << 2 | gpio_get(A1_PIN) << 1 | gpio_get(A0_PIN) << 0;
+uint8_t read_addr_low_byte() {
+    uint8_t value = 0;
+    for (size_t i = 0; i < ADDR_PINS.size(); ++i) {
+        value |= gpio_get(ADDR_PINS[i]) << i;
+    }
+    return value;
 }
 
 [[noreturn]] int main() {
@@ -31,14 +37,20 @@ uint8_t read_addr_nibble() {
     gpio_set_function(TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(RX_PIN, GPIO_FUNC_UART);
 
+    uart_set_format(uart0, 8, 1, UART_PARITY_NONE);
+    uart_set_fifo_enabled(uart0, false);
+
+    sleep_ms(200);  // let UART settle
+
     init_addr_pins();
 
-    uart_puts(uart0, "7800 address probe starting...\r\n");
+    uart_puts(uart0, "7800 address probe starting (A0–A7)...\r\n");
 
+    std::array<char, 80> line;
     while (true) {
-        std::array<char, 80> line;
-        const uint8_t addr = read_addr_nibble();
-        snprintf(line.data(), sizeof(line), "A[3:0] = %X\r\n", addr);
+        const uint8_t addr = read_addr_low_byte();
+        snprintf(line.data(), line.size(), "A[7:0] = %02X\r\n", addr);
         uart_puts(uart0, line.data());
+        sleep_ms(50);
     }
 }
